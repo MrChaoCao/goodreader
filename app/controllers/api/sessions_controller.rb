5845b7b29b5ba0cde912ccb2219a98ebc4cfd151
chao <mailbox.chao.cao@gmail.com>
2018-04-05 15:32:11 -0700

    Immprove codestyle

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,12 +1,11 @@
 class Api::SessionsController < ApplicationController
   def create
     @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
-    debugger
     if @user
       login(@user)
-      render "api/users/show"
+      render 'api/users/show'
     else
-      render json: ["Invalid username/password combination"], status: 401
+      render json: ['Invalid username/password combination'], status: 401
     end
   end
 
