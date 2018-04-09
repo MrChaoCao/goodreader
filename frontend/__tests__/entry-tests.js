0024a0c396aed43c875bd73b56d58faf1a528102
chao <mailbox.chao.cao@gmail.com>
2018-04-09 14:01:19 -0700

    Add more frontend tests for the entry file

--- frontend/__tests__/entry-tests.js
+++ frontend/__tests__/entry-tests.js
@@ -4,7 +4,7 @@ import configureStore from '../store/store';
 jest.mock('../store/store', () => jest.fn(() => ({ storeKey: 'storeValue' }) ));
 
 describe('entry', () => {
-  let Entry, ReactDOM, Root;
+  let Entry, ReactDOM, Root, renderedRoot;
 
   beforeAll(() => {
     document.addEventListener = jest.fn();
@@ -20,10 +20,29 @@ describe('entry', () => {
     document.addEventListener.mock.calls[0][1]();
   });
 
+  it('sets a listener for the DOMContentLoaded event', () => {
+    const eventListenerCalls = document.addEventListener.mock.calls;
+
+    expect(document.addEventListener).toBeCalled();
+    expect(eventListenerCalls[0][0]).toEqual('DOMContentLoaded');
+  });
+
+  it ('renders the Root component', () =>{
+    renderedRoot = ReactDOM.render.mock.calls[0][0];
+    expect(renderedRoot.type).toEqual(Root.default)
+  });
+
+  it('queries for and renders into the root div', () => {
+    expect(document.getElementById).toBeCalledWith("root");
+    expect(ReactDOM.render.mock.calls[0][1]).toEqual("root");
+  })
+
   it('invokes the configureStore function', () => {
     expect(configureStore).toBeCalled();
   });
 
-  // Add more tests here!
-
+  it('passes the store as a prop to the Root component', () => {
+    renderedRoot = ReactDOM.render.mock.calls[0][0];
+    expect(renderedRoot.props.store).toEqual({ storeKey: 'storeValue' });
+  })
 });
