3dbea2790506d04d78a28c912fac9171b62de012
chao <mailbox.chao.cao@gmail.com>
2018-04-11 14:23:33 -0700

    Add frontend auth/protected routes and route switch

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import {
+  Route,
+  Redirect,
+  Switch,
+  Link,
+  HashRouter
+} from 'react-router-dom';
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
 
 const App = () => (
   <div>
-    <h1>Goodreader</h1>
+    <Link to="/" className="">
+      <h1>goodreader</h1>
+    </Link>
+
+    <Switch>
+      <Route exact path="/"/>
+      <Route render={ () => <Redirect to="/"/> } />
+    </Switch>
   </div>
 )
 
